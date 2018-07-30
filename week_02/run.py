8076fe7210082d5d21d4803fdfb58c6974e77b0b
Emory Fierlinger <emory.fierlinger@gmail.com>
2018-07-31 11:36:39 +1200

    Added week 2 files

---
+++ week_02/run.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+
+# to run:
+# cd to directory
+# source env/bin/activate
+# export FLASK_APP=run.py; export FLASK_DEBUG=1
+# flask run
+
+MENUDB = 'menu.db'
+db = sqlite3.connect(MENUDB)
+cursor = db.execute('SELECT * FROM burgers')
+print(cursor)
+for row in cursor:
+    print(row)
+
+burgers = [
+ ['Classic Burger', '$4.99'],
+ ['Cheese Burger', '$5.99'],
+ ['Chicken Burger', '$5.99'],
+ ['Double Burger', '$6.99']
+]
+
+drinks = [
+ ['Cola', '$0.99'],
+ ['Ginger Ale', '$0.99'],
+ ['Beer', '$1.99'],
+ ['Coffee', '$1.99']
+]
+
+sides = [
+ ['Fries', '$1.99'],
+ ['Onion Rings', '$1.99'],
+ ['Mushrooms', '$1.99'],
+ ['Salad', '$1.99']
+]
+
+@app.route('/')
+def index():
+  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
+
+@app.route('/order')
+def order():
+  return render_template('order.html')
