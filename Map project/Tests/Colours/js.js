d8a7acc278a399d2795a689380eb5677b6222d58
Emory Fierlinger <emory.fierlinger@gmail.com>
2018-10-18 22:55:17 +1300

    Final project submission

--- Map project/Tests/Colours/js.js
+++ Map project/Tests/Colours/js.js
@@ -18,11 +18,9 @@ var Places = [
                 
             ];
 
-var done = false;
-
 var highestprice = 0;
 
-currentlySelected = false;
+var currentlySelected = false;
 
 function getColor(value){
     //value from 0 to 1
@@ -31,10 +29,19 @@ function getColor(value){
 };
 
 function TC(str) {
-                return str.replace(/\w\S*/g, function(txt){
+                
+            }
+
+function formatText(str) {
+    
+    str = str.replace(/_/g, " ",)
+    
+    str = str.replace(/\w\S*/g, function(txt){
                     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
                 });
-            }
+    return str.replace(/Of/g, "of"); //Article decapitalize – grammar!
+    
+}
 
 function paintAll() {
     Places.forEach(function(place) {
@@ -43,7 +50,7 @@ function paintAll() {
             } else if (place[1] == 0){
                 place[0].style.fill = '#666'
             } else if (place[1] == -1) {
-                place[0].style.fill = "purple"
+                place[0].style.fill = "white"
             }
     })
 }
@@ -56,6 +63,10 @@ function reset() {
     })
     
     document.getElementById("title").innerHTML = "Select a location to get started"
+    
+    document.getElementById("place_name").innerHTML = "";
+    document.getElementById("price").innerHTML = "";
+    
 }
 
 Places.forEach(function(place) {
@@ -63,31 +74,32 @@ Places.forEach(function(place) {
         
         if (!currentlySelected) {
             
-            place[0].style.fill = "purple";
+            place[0].style.fill = "white";
             
             var currentPlace = place[2]
         
-        currentlySelected = true;
-        
-        document.getElementById("title").innerHTML = "Currently selected origin: " + TC(place[0].id).replace("_", " ");
-        
-        document.getElementById("exit-button").style.display = "block"
-        
-        document.getElementById("exit-button").addEventListener("click", function() {
-            reset()
-            currentlySelected = false;
-        })
-        
-        
-        highestprice = 0;
-        for (var i = 0; i < Places.length; i++) {
+            currentlySelected = true;
+
+            document.getElementById("title").innerHTML = "Currently selected origin: " + formatText(place[0].id);
+
+            document.getElementById("exit-button").style.display = "block"
             
-            Places[i][1] = 0; 
-            Places[i][0].style.fill="black";
-            makeRequest(currentPlace, i, place);
-        }
-        
-        done = true;
+            document.getElementById("place_name").innerHTML = "Now tap any of the coloured locations to see price data"
+
+            document.getElementById("exit-button").addEventListener("click", function() {
+                reset()
+                currentlySelected = false;
+            })
+
+
+            highestprice = 0; //reset
+            for (var i = 0; i < Places.length; i++) {
+
+                Places[i][1] = 0; 
+                makeRequest(currentPlace, i, place);
+            }
+            
+            place[1] = -1;
             
         }
         
@@ -96,13 +108,25 @@ Places.forEach(function(place) {
     
     place[0].addEventListener('mouseover', function() {
         
-        if (done) {
+        if (currentlySelected) {
             place[0].style.fill="blue"
+            
             console.log(place[1])
-            document.getElementById("price").innerHTML = place[1];
+            
+            document.getElementById("place_name").innerHTML = "Destination: " + formatText(place[0].id);
+            
+            if (place[1]>0) {
+                document.getElementById("price").innerHTML = "Cost of one-way air travel with Air New Zealand: $" + place[1];
+            } else {
+                document.getElementById("price").innerHTML = "No flights were found between these locations"
+            }
+            
+            
+            
+            
         } else {
-            this.style.fill='blue';
-            document.getElementById("title").innerHTML = TC(place[0].id).replace("_", " ");
+            this.style.fill='white';
+            document.getElementById("title").innerHTML = "Hovered location: " + formatText(place[0].id);
         }
         
         
@@ -111,18 +135,15 @@ Places.forEach(function(place) {
     
     place[0].addEventListener('mouseout', function() {
         
-        if (done) {
-            document.getElementById("price").innerHTML = "price";
+        if (currentlySelected) {
             
             if (place[1] > 0) {
                 place[0].style.fill = getColor(place[1]/highestprice)
             } else if (place[1] == 0){
                 place[0].style.fill = '#666'
             } else if (place[1] == -1) {
-                place[0].style.fill = "purple"
-            }
-            
-            //place[0].style.fill = getColor(place[1]/highestprice);
+                place[0].style.fill = "white"
+            };
             
         } else {
             this.style.fill='black';
@@ -156,7 +177,7 @@ function makeRequest(originCode, desCode, place) {
 
                 Places[desCode][1] = APIdata.prices[0].adultPriceIncludingTax;
 
-                place[0].style.fill = "purple";
+                place[0].style.fill = "white";
                 place[1] = -1
 
                 if (Places[desCode][1] > highestprice){
@@ -168,7 +189,8 @@ function makeRequest(originCode, desCode, place) {
                 } else if (this.status == 400) {
                     price[1] = -20;
 
-                } else if (this.status == 503) {
+                } else if (this.status == 503 || this.status == 502 || this.status == 504) {
+                    console.log("The API has reached capacity – wait 10-15 minutes to get let back in")
                 }
 
                 });
